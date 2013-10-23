7114c184b56729491959b12d2868da63df5f54b3
ramya-machina <machina_ramya@yahoo.com>
2013-10-23 09:46:31 -0700

    MongoDB integration

--- src/main/java/edu/sjsu/conference/controller/RegisterParticipantController.java
+++ src/main/java/edu/sjsu/conference/controller/RegisterParticipantController.java
@@ -1,6 +1,7 @@
 package edu.sjsu.conference.controller;
 
 import javax.validation.Valid;
+import java.net.UnknownHostException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -9,16 +10,33 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import org.apache.log4j.Logger;
 
 import edu.sjsu.conference.domain.Participant;
+import edu.sjsu.conference.repository.ParticipantRepository;
+import edu.sjsu.conference.config.MongoConfig;
+
+import com.mongodb.Mongo;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/registerParticipant")
 //@SessionAttributes("participant")
 
 public class RegisterParticipantController {
-	
-
+		//Code related to MongoDB [START]
+		@Autowired
+    	private ParticipantRepository participantRepository;
+		
+		protected static Logger log = Logger.getLogger("RegisterParticipantController");
+		//Code related to MongoDB [END]
+		
 		@RequestMapping(method = RequestMethod.GET)
 	    public String setupRegistration(@ModelAttribute("participant") Participant participant1 , BindingResult participant) {
 	        //model.addAttribute("user", new User());
@@ -31,9 +49,30 @@ public class RegisterParticipantController {
 	        if (result.hasErrors()) {
 	            return "registerParticipant";
 	        }
+
+	        //Code related to MongoDB [START]
+	        log.debug("registerParticipant() : Participant = "+participant.toString());
+
+	        AbstractApplicationContext context = new AnnotationConfigApplicationContext(MongoConfig.class);
+        	ParticipantRepository repository = context.getBean(ParticipantRepository.class);
+	        
+	        //Create collection and insert into it.
+	        repository.addParticipant(participant);
+
+	        // Display all the documents from the collection
+	        List<Participant> part = repository.listParticipant();
+	        for (int i=0;i<part.size();i++)
+	        	System.out.println("Ramya firstname:"+part.get(i));
+
+	        //Remove documents from the collection
+	        //repository.deleteParticipant("l@l");
+
+	        //Code related to MongoDB [END]
+
 	        model.addAttribute("particpantFirstName", participant.getFirstName());
 	        model.addAttribute("particpantLastName", participant.getLastName());
 	        model.addAttribute("participantLocation", participant.getLocation());
+    
 			return "success";
 	        
 	    }
--- src/main/java/edu/sjsu/conference/controller/SpeakerController.java
+++ src/main/java/edu/sjsu/conference/controller/SpeakerController.java
@@ -1,6 +1,7 @@
 package edu.sjsu.conference.controller;
 
 import javax.validation.Valid;
+import org.apache.log4j.Logger;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -8,15 +9,27 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
+import edu.sjsu.conference.config.MongoConfig;
 import edu.sjsu.conference.domain.Participant;
 import edu.sjsu.conference.domain.Speaker;
+import edu.sjsu.conference.repository.SpeakerRepository;
 
 
 @Controller
 @RequestMapping("/SpeakerPage")
 
 public class SpeakerController {
+
+	//Code related to MongoDB [START]
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    protected static Logger log = Logger.getLogger("RegisterParticipantController");
+	//Code related to MongoDB [END]
 	
 	@RequestMapping(method = RequestMethod.GET)
     public String setupRegistration(@ModelAttribute("speaker") Speaker speaker1 , BindingResult speaker) {
@@ -25,10 +38,22 @@ public class SpeakerController {
     }
 
 	@RequestMapping(method = RequestMethod.POST)
-    public String registerParticipant( @ModelAttribute("speaker") @Valid Speaker speaker1, BindingResult result, ModelMap model) {
+    public String registerSpeaker( @ModelAttribute("speaker") @Valid Speaker speaker1, BindingResult result, ModelMap model) {
         if (result.hasErrors()) {
-            return "registerParticipant";
+            return "registerSpeaker";
         }
+
+        //Code related to MongoDB [START]
+        log.debug("registerSpeaker() : speaker = "+speaker1.toString());
+
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(MongoConfig.class);
+        SpeakerRepository repository = context.getBean(SpeakerRepository.class);
+        
+        //Create collection and insert into it.
+        repository.addSpeaker(speaker1);
+
+        //Code related to MongoDB [END]
+
         model.addAttribute("SpeakerFirstName", speaker1.getFirstName());
         model.addAttribute("SpeakerLastName", speaker1.getLastName());
         model.addAttribute("SpeakerLocation", speaker1.getLocation());
