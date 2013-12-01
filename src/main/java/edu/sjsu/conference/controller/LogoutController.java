1df3756ca0948ee6e5270ff2249c72353b64c7c7
sarvagyaj <sarvagyaj@gmail.com>
2013-12-01 12:30:36 -0800

    Added Logout functionality

---
+++ src/main/java/edu/sjsu/conference/controller/LogoutController.java
@@ -0,0 +1,23 @@
+package edu.sjsu.conference.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/Logout")
+public class LogoutController {
+
+	protected static Logger log = Logger.getLogger("LogoutController");
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String logout(HttpSession session) {
+		log.debug("Logout Controller : Will logout now");
+		session.invalidate();
+		return "redirect:/LoginPage";
+	}
+
+}
